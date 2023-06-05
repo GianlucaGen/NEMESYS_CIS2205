6042b073c1a48739ab8bd3893abdfaf4abc2247f
LOLIPOP1136 <117951436+LOLIPOP1136@users.noreply.github.com>
2023-06-05 22:48:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using aspnet_blog_application.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(300);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
 
 var app = builder.Build();
 
@@ -23,5 +32,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Posts}/{action=Index}/{id?}");
+
+app.UseSession();
 
 app.Run();
